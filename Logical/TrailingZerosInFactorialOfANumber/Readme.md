5b564d6bcfad6d5c57e294f9bc197acce84f3e2b
Banothu Ramesh Naik <rameshnaik5521@gmail.com>
2020-01-07 21:49:46 +0530

    Logical - Trailing Zeros in Factorial of a given number

---
+++ Logical/TrailingZerosInFactorialOfANumber/Readme.md
@@ -0,0 +1,43 @@
+# Trailing Zeros Easy
+
+Count the number of trailing 0s in factorial of a given number.
+
+## Input Format
+
+First line of input contains T - number of test cases. Its followed by T lines, each containing an integer N.
+
+## Constraints
+
+```shell
+1 <= T <= 10000
+1 <= N <= 1015
+```
+
+## Output Format
+
+For each test case, print the number of trailing 0s in N!, separated by new line.
+
+## Sample Input 0
+
+```shell
+3
+2
+5
+20
+```
+
+## Sample Output 0
+
+```shell
+0
+1
+4
+```
+
+## Explanation 0
+
+Self Explanatory
+
+## Ref Link
+
+[Trailing Zeros Easy](https://www.hackerrank.com/contests/smart-interviews/challenges/si-trailing-zeros/problem)
---
+++ Logical/TrailingZerosInFactorialOfANumber/solution-nodejs/solution.js
@@ -0,0 +1,46 @@
+// https://www.hackerrank.com/contests/smart-interviews/challenges/si-rotation-of-matrix
+
+
+/** 
+ * Number of zeros of any number will have factor of 5
+ * 
+ * Explanation:
+ * N = 121
+ * 
+ * 121//5 = 24
+ * 121/25 = 4
+ * 121/125 = 0
+ * 
+ * ====
+ * 28
+*/
+
+function findTrailingZeros(N) {
+    // Number of zeros of any number will have factor of 5
+    let div = 5;
+    let count = 0;
+    while(Math.floor(N/div) != 0) {
+        count += Math.floor(N/div);
+        div *= 5;
+    }
+    return count;
+}
+
+function processData(input) {
+    // Enter your code here
+    // Break input with \n let say inputArray
+    const inputArray = input.split("\n");
+    // Number of test cases T = inputArray[0]
+    let T = inputArray[0];
+    // Loop until we reach number of test cases
+    let nIndex = 1;
+    while (T-- && inputArray[nIndex] != "") {
+        // get number of elements N = inputArray[nIndex]
+        let N = inputArray[nIndex++];
+        const count = findTrailingZeros(N);
+        console.log(count);
+    }
+};
+
+const input = "6\n2\n5\n20\n30\n100\n121\n";
+processData(input);
